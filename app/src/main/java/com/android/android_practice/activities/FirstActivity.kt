4b89daf700a7dc1d33a79d39ad9539fe017a2f3a
Oscar Ekesiobi <oscarnipps@gmail.com>
2023-10-25 22:25:28 +0200

    activity practice

--- app/src/main/java/com/android/android_practice/activities/FirstActivity.kt
+++ app/src/main/java/com/android/android_practice/activities/FirstActivity.kt
@@ -1,7 +1,10 @@
 package com.android.android_practice.activities
 
+import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.android.android_practice.R
 import timber.log.Timber
 
@@ -10,6 +13,37 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         Timber.d("onCreate called")
+        Timber.d(savedInstanceState?.getString("stateBundleKey"))
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Timber.d("landscape mode !!")
+        }
+
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Timber.d("portrait mode !!")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Timber.d("onSaveInstanceState called")
+        outState.putString("stateBundleKey" , "state being used")
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Timber.d("onRestoreInstanceState called")
+        Timber.d(savedInstanceState.getString("stateBundleKey"))
+    }
+
+    fun proceedToSecondActivity(view: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onStart() {
@@ -25,7 +59,6 @@ class FirstActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Timber.d("onResume called")
-        Timber.d("")
     }
 
     override fun onPause() {
---
+++ app/src/main/java/com/android/android_practice/activities/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.android.android_practice.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.android_practice.R
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
